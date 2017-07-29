0bb8330f2278cc48c7c697529704b2f251d2d561
d.danevskyi <d.danevskyi@gmail.com>
2017-07-29 13:27:30 +0300

    Update FullyConnectedClassifier (add train/evaluation methods)

--- networks/network_dense.py
+++ networks/network_dense.py
@@ -1,20 +1,19 @@
 from typing import Union
 
 import tensorflow as tf
+from tqdm import tqdm
 
+from networks.network_base import BaseNetwork
+from utils import tensorflow_utils
 
-def get_second_dimension(tensor):
-    return tensor.get_shape().as_list()[1]
-
-
-class FullyConnectedClassifier:
+class FullyConnectedClassifier(BaseNetwork):
 
     def __init__(self,
                  input_size: int,
                  n_classes: int,
                  layer_sizes: list,
                  activation_fn=tf.nn.relu,
-                 dropout=0.75,
+                 dropout=0.25,
                  momentum=0.9,
                  scope='FullyConnectedNetwork'):
 
@@ -39,6 +38,10 @@ class FullyConnectedClassifier:
                 self.train_op = self._create_optimizer(self.loss,
                                                        learning_rate=self.learning_rate,
                                                        momentum=momentum)
+                self._create_metrics(logits=self.logits,
+                                     labels=self.labels,
+                                     loss=self.loss)
+                self.init_variables(tf.global_variables())
 
 
     def _create_placeholders(self):
@@ -47,7 +50,7 @@ class FullyConnectedClassifier:
                                      shape=(None, self.input_size),
                                      name='inputs')
     
-        self.labels = tf.placeholder(dtype=tf.int32,
+        self.labels = tf.placeholder(dtype=tf.int64,
                                      shape=None,
                                      name='labels')
     
@@ -73,7 +76,7 @@ class FullyConnectedClassifier:
     
                 with tf.variable_scope('layer_{layer}'.format(layer=i+1)):
                     name = 'weights'
-                    shape = (get_second_dimension(net), layer_size)
+                    shape = (tensorflow_utils.get_second_dimension(net), layer_size)
                     weights = tf.get_variable(name=name, shape=shape)
         
                     name = 'bias'
@@ -111,24 +114,75 @@ class FullyConnectedClassifier:
                                                    momentum=momentum,
                                                    name='optimizer')
             global_step = tf.Variable(0)
-            train_op = optimizer.minimize(loss, global_step=global_step)
+            train_op = optimizer.minimize(loss, global_step=global_step, name='train_op')
 
             return train_op
 
+    def _create_metrics(self,
+                        logits: tf.Tensor,
+                        labels: tf.Tensor,
+                        loss: tf.Tensor):
+
+        correct_prediction = tf.equal(tf.argmax(self.logits, 1), labels)
+        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     def fit(self,
             n_epochs: int,
+            batch_size: int,
             learning_rate_schedule: callable,
             train_data_provider,
-            validation_data_provider):
+            validation_data_provider,
+            test_data_provider):
+
+        n_iterations = train_data_provider.num_examples // batch_size
 
         for epoch in range(n_epochs):
+            print('Starting epoch {epoch}.\n'.format(epoch=epoch+1))
+            for iteration in tqdm(range(n_iterations), ncols=75):
 
-            pass
+                images, labels = train_data_provider.next_batch(batch_size)
 
+                feed_dict = {self.inputs: images,
+                             self.labels: labels,
+                             self.learning_rate: learning_rate_schedule(epoch+1),
+                             self.keep_prob: 1 - self.dropout} 
 
+                self.sess.run(self.train_op, feed_dict=feed_dict)
+    
+            train_accuracy, train_loss = self.evaluate(train_data_provider,
+                                                       batch_size=batch_size)
+            validation_accuracy, validation_loss = self.evaluate(train_data_provider,
+                                                                 batch_size=batch_size)
 
+            print('\nEpoch {epoch} completed.'.format(epoch=epoch+1))
+            print('Accuracy on train: {accuracy}, loss on train: {loss}'.format(
+                                    accuracy=train_accuracy, loss=train_loss))
+            print('Accuracy on validation: {accuracy}, loss on validation: {loss}'.format(
+                                    accuracy=validation_accuracy, loss=validation_loss))
 
+    def evaluate(self, data_provider, batch_size: int):
+
+        fetches = [self.accuracy, self.loss]
+
+        n_iterations = data_provider.num_examples // batch_size
+
+        average_accuracy = 0
+        average_loss = 0
+
+        for iteration in range(n_iterations):
+
+            images, labels = data_provider.next_batch(batch_size)
+
+            feed_dict = {self.inputs: images,
+                         self.labels: labels,
+                         self.keep_prob: 1.0} 
+
+            accuracy, loss = self.sess.run(fetches, feed_dict=feed_dict)
+            
+            average_accuracy += accuracy / n_iterations
+            average_loss += loss / n_iterations
+
+        return average_accuracy, average_loss
 
     def predict():
 
