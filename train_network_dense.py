6d2117afcbcd17380d04268900ec56b37433580e
d.danevskyi <d.danevskyi@gmail.com>
2017-07-29 13:26:58 +0300

    Add script for training dense network

---
+++ train_network_dense.py
@@ -0,0 +1,19 @@
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/")
+
+train_data_provider = mnist.train
+validation_data_provider = mnist.validation
+test_data_provider = mnist.test
+
+from networks import network_dense
+from configs import ConfigNetworkDense as config
+
+classifier = network_dense.FullyConnectedClassifier(input_size=config.input_size,
+                                                    n_classes=config.n_classes,
+                                                    layer_sizes=config.layer_sizes)
+classifier.fit(n_epochs=config.n_epochs,
+               batch_size=config.batch_size,
+               learning_rate_schedule=config.learning_rate_schedule,
+               train_data_provider=train_data_provider,
+               validation_data_provider=validation_data_provider,
+               test_data_provider=test_data_provider)
